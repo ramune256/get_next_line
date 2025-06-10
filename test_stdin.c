00852089161a01de2792ecea910d4beea84da96b
ramune256 <shungo180620@outlook.jp>
2025-06-10 16:27:43 +0900

    added test_stdin.c

---
+++ test_stdin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_stdin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:23:36 by shunwata          #+#    #+#             */
+/*   Updated: 2025/06/10 16:25:25 by shunwata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	main(void)
+{
+	char	*line;
+
+	while ((line = get_next_line(0)) != NULL)
+	{
+	printf("入力: [%s]\n", line);
+	free(line);
+	}
+	return (0);
+}
